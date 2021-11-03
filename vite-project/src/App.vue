90c0abb739e5c5b130b110eceb68473f4528c5e8
Meganenou <mmoorghen@et.esiea.fr>
2021-11-03 12:51:28 +0100

    change tabulations mode

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -5,19 +5,19 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 3 + Vite + <3" />
-  </div>
+	<div>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello Vue 3 + Vite + <3" />
+	</div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
